ca2171e14d006733503fed418a6de693d0bb645f
vishalchillal <vishal.k3gster@gmail.com>
2016-07-15 22:51:27 +0000

    the cycle of signing up is complete now.

--- new UI Part/js/myScript.js
+++ new UI Part/js/myScript.js
@@ -4,13 +4,16 @@
 }
 function login() {
     $(".register").css("visibility", "hidden");
-    $(".logIn").css("visibility", "visible");
+    $(".signIn").css("visibility", "visible");
+    $(".selectUsername").css("visibility", "hidden");
 }
 function selectUsrName() {
     $(".selectUsername").css("visibility", "visible");
     $(".register").css("visibility", "hidden");
 }
-
 function newDoc() {
     window.location.assign("http://www.w3schools.com")
+}
+function newDoc1() {
+    window.location.assign("logIn.html")
 }
