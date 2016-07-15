461d9c12f95aa3c3cf4b1bac24f678b1a7562a46
vishalchillal <vishal.k3gster@gmail.com>
2016-07-15 22:23:40 +0000

    added panal containing create username & password.

--- new UI Part/js/myScript.js
+++ new UI Part/js/myScript.js
@@ -1,6 +1,16 @@
-$(document).ready(function (){
-    $('#btnlogIn').click(function(e){
-	$('#logIn').visible="false"
-    }
+﻿function register() {
+    $(".register").css("visibility", "visible");
+    $(".logIn").css("visibility", "hidden");
+}
+function login() {
+    $(".register").css("visibility", "hidden");
+    $(".logIn").css("visibility", "visible");
+}
+function selectUsrName() {
+    $(".selectUsername").css("visibility", "visible");
+    $(".register").css("visibility", "hidden");
+}
 
-});
+function newDoc() {
+    window.location.assign("http://www.w3schools.com")
+}
