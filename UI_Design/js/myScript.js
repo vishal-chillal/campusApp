188bfa8c6267dcdb4d2c6f34a7b5d6fa0dc09052
vishalchillal <vishal.k3gster@gmail.com>
2016-07-27 13:23:52 +0000

    change the folder name and done something coo, in UI page.

--- UI_Design/js/myScript.js
+++ UI_Design/js/myScript.js
@@ -1,4 +1,9 @@
-﻿function register() {
+﻿
+$(function () {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+function register() {
     $(".register").css("visibility", "visible");
     $(".logIn").css("visibility", "hidden");
 }
@@ -10,6 +15,14 @@ function login() {
 function selectUsrName() {
     $(".selectUsername").css("visibility", "visible");
     $(".register").css("visibility", "hidden");
+
+}
+function hideNav() {
+    $(".Header").css("visibility","hidden");
+}
+
+function showNav() {
+    $(".Header").css("visibility","visible");
 }
 function newDoc() {
     window.location.assign("http://www.w3schools.com")
