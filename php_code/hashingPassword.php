e3629384297dd4d502ebeafa704eda9f799d36c8
Harshal Chaudhari <harshal.chaudhari@live.com>
2016-07-28 17:31:13 +0530

    php: Added more secure hashing function

--- php_code/hashingPassword.php
+++ php_code/hashingPassword.php
@@ -1,19 +1,46 @@
 <?php 
 	#encryptPass is used to encrypt user entered password.
 	function encryptPass($userEnteredPass){
-		return password_hash(str_rot13($userEnteredPass), PASSWORD_BCRYPT);
+		return password_hash($userEnteredPass, PASSWORD_BCRYPT);
 	}
 
 	#verifyPass can be used to verify that user entered pass and pass from db are matched.
 	function verifyPass($userEnteredPass,$encryptedPassFromDB){
-		if (password_verify(str_rot13($userEnteredPass), $encryptedPassFromDB)) {
+		if (password_verify($userEnteredPass, $encryptedPassFromDB)) {
 			return 1;
 		}
 		else{
 			return -1;
 		}
 	}
-	#Example for better understanding
-	$encPass = encryptPass("Harshal"); 
-	echo verifyPass("Harshal",$encPass);
+
+	#More secure way to store password but in this we have to store salt and secret key for each user in db
+	#function for encrypting password
+	function getSecureHashing($userEnteredPass){
+		$salt = '12061994'; #this will be generated using rand() function. and we will store to db for each user
+		$iterations = 20000;
+		$secretKey = 'ga22me'; #this can be same or different for each user.
+		$hash = hash_pbkdf2("sha256", $userEnteredPass, $salt, $iterations,20);
+		$encryptedPassword = hash_hmac('ripemd160', $hash, $secretKey);
+		return $encryptedPassword;
+	}
+
+	#function for decrypting password
+	function verifySecureHashing($userEnteredPass,$encryptedPassFromDB){
+		$salt = '12061994'; #this will be retrieved from db
+		$iterations = 20000;
+		$secretKey = 'ga22me'; #this will be retrieved from db
+		$hash = hash_pbkdf2("sha256", $userEnteredPass, $salt, $iterations,20);
+		$userPassEncrypted = hash_hmac('ripemd160',$hash, $secretKey);
+		if ($userPassEncrypted == $encryptedPassFromDB){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	}
+
+	$userEnteredPass = "harshal";
+	$encryptedPassFromDB = getSecureHashing("harshal");
+	echo verifySecureHashing($userEnteredPass,$encryptedPassFromDB);
 ?>
