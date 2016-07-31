ae114ef9eb1d07a69ecec45408dec24f410fb5ef
saurabhkukade <saurabhkukade@livecom>
2016-07-31 18:29:19 +0530

    added file upload readme and fixed some bugs

--- admin/file_upload/upload.php
+++ admin/file_upload/upload.php
@@ -1,10 +1,7 @@
 <?php
-
-$target_dir = 'uploads/';
+$target_dir = 'uploads/'; //directory for saving uploaded file
 $target_file = $target_dir.basename($_FILES['fileToUpload']['name']);
-$uploadOk = 1;
 $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
 if (isset($_POST['submit'])) {
     print_r($_FILES['fileToUpload']['tmp_name']);
     if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
